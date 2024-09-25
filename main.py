12b6322f9699e5e3eed656f9b507ef5e810cff45
MaryamKalantarii <maryamkalantaria72@gmail.com>
2024-09-25 19:34:37 +0330

    path & query parameter

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# first test
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
+# Query parameter
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+# path parameter
+@app.get("/home/{name}/{age}")
+def index(name:str,age:int):
+    return {"messege": f"Hello, {name}! Your age is {age}."}
